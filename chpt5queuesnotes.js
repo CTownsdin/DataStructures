a47e448e26c5e6169e25d04edf15f36a682d1bc0
CTownsdin <Townsdin@gmail.com>
2014-11-07 07:55:27 -0800

    Add Q3_PQ and modify notes file

--- chpt5queuesnotes.js
+++ chpt5queuesnotes.js
@@ -5,25 +5,25 @@ function Queue() {
 }
 Queue.prototype.enqueue = function (element) {
     this.dataStore.push(element);
-}
+};
 Queue.prototype.dequeue = function () {
     return this.dataStore.shift();
-}
+};
 Queue.prototype.front = function () {
     return this.dataStore[0];
-}
+};
 Queue.prototype.back = function () {
     return this.dataStore[this.dataStore.length-1];
-}
+};
 Queue.prototype.toString = function () {
     var retStr = "";
     for (var i = 0; i < this.dataStore.length; ++i) {
         retStr += this.dataStore[i] + "\n"; }
         return retStr;
-    }
-    function empty() {
+};
+Queue.prototype.empty = function () {
         if (this.dataStore.length === 0) { return true; }
         else { return false; }
-}
+};
 
 
