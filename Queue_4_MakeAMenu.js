b0ae8089fe77c3867f416a1b24d5b64a4e4f6871
CTownsdin <Townsdin@gmail.com>
2014-11-07 17:33:23 -0800

    Add more exercises, semi-completed

---
+++ Queue_4_MakeAMenu.js
@@ -0,0 +1,6 @@
+// TODO:?
+// 4. Modify the ED example (Example 5-5) so the user can control the activity in the ED. Create a menu system that allows the user to choose from the following activities:
+//      a. Patient enters ED.
+//      b. Patient is seen by doctor.
+//      c. Display list of patients waiting to be seen.
+// //
